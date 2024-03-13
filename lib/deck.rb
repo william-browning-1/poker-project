482121cf0f39546cbe5b5f2d048fcf5559b485f0
Will Browning <wdbrwnng@memphis.edu>
2024-03-13 11:56:32 -0500

    Refactoring/Bookmark/Stopping Place

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,23 +2,17 @@ require_relative 'Card'
 
 class Deck < Card
 
-  def create_deck
-    deck = []
-
+  def initilize
+    @deck = []
     ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
     suites = ['hearts', 'clubs', 'spades', 'diamonds']
-    card_num = 0
 
     ranks.each do |card_rank|
       suites.each do |card_suite|
-        deck.push(Card.new.identity(card_rank, card_suite))
-        card_num += 1
+        @deck.push(Card.new.identity(card_rank, card_suite))
       end
     end
-    card_num
+    @deck.shuffle!
   end
 
 end
-
-Deck_1 = Deck.new
-Deck_1.create_deck
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,9 +2,9 @@ require 'Deck'
 
 RSpec.describe Deck do
   let(:deck) {Deck.new}
-  describe ".num" do
-    it "has a correct number of cards in the deck" do
-      expect(deck.create_deck).to eq(52)
+  describe ".shuffle_deck" do
+    it "shuffles the cards in the deck" do
+      expect(deck.compare(deck)).to be False
     end
   end
 end
