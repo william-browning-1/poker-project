1871eacb2e2bb1422a19a6176292cad0eb28d22e
Will Browning <wdbrwnng@memphis.edu>
2024-03-13 15:43:43 -0500

    Made some changes to tests, created random_card function with some test for it

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,18 +1,33 @@
 require_relative 'Card'
 
 class Deck < Card
-    $deck = []
-    $ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
-    $suites = ['hearts', 'clubs', 'spades', 'diamonds']
+    attr_reader :deck, :hand
+    def initialize
+      @hand = hand
+      @deck = []
+      ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace']
+      suites = ['hearts', 'clubs', 'spades', 'diamonds']
 
-    $ranks.each do |card_rank|
-      $suites.each do |card_suite|
-        $deck.push(Card.new.identity(card_rank, card_suite))
+      ranks.each do |card_rank|
+        suites.each do |card_suite|
+          @deck.push(Card.new.identity(card_rank, card_suite))
+        end
       end
     end
 
     def shuffle_deck
-      $deck.shuffle()
-      $deck.length
+      @deck.shuffle
+      @deck.length
+    end
+
+    def random_card
+      @hand = []
+      [0,1,2,3,4].each do |cards|
+        card_1 = rand(0...51)
+        @hand << @deck[card_1]
+        @deck.delete(@deck[card_1])
+      end
+      @hand.length
+      @deck.length
     end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,12 +1,21 @@
 require 'Deck'
 
 RSpec.describe Deck do
-  let(:deck) {Deck.new}
+  let(:deck1) {Deck.new}
   describe ".shuffle_deck" do
     it "has the correct number of cards in the deck" do
-      expect(deck.shuffle_deck).to eq(52)
+      deck1.shuffle_deck
+      expect(deck1.deck.length).to eq(52)
     end
   end
 
-
+  describe ".random_card" do
+    it "correct number of cards are dealt" do
+      deck1.random_card
+      expect(deck1.hand.length).to eq(5)
+    end
+    it "Takes cards in hand out of the deck" do
+      expect(deck1.random_card).to eq(47)
+    end
+  end
 end
